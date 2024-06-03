4eb32624fea25ae7c5b04eaa00429f397f7462ba
olaussen123 <mathias.olaussen@live.no>
2024-06-03 22:37:54 +0200

    first

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 import "./globals.css";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
